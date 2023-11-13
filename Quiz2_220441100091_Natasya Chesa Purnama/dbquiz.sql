c7e88bd3c3f0f0241a3553edd08aebb052c6d266
Natasya Chesa Purnama <103979457+NatasyaPurnama@users.noreply.github.com>
2023-11-13 13:22:05 +0700

    Add files via upload

---
+++ Quiz2_220441100091_Natasya Chesa Purnama/dbquiz.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Nov 2023 pada 07.18
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbquiz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`) VALUES
+(1, 'natasya', '091');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Quiz2_220441100091_Natasya Chesa Purnama/src/quiz2/Koneksi.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz2;
+
+import com.mysql.jdbc.Connection;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Natasya
+ */
+public class Koneksi {
+
+    private static Connection mysqlkonek;
+
+    public static Connection koneksiLogin() throws SQLException {
+        if (mysqlkonek == null) {
+            try {
+                String DB = "jdbc:mysql://localhost:3306/dbquiz";
+                String user = "root";
+                String pw = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                mysqlkonek = (Connection) DriverManager.getConnection(DB, user, pw);
+                JOptionPane.showMessageDialog(null, "Koneksi berhasil");
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Koneksi gagal: " + e.getMessage());
+            }
+        }
+        return mysqlkonek;
+    }
+
+    public static void main(String[] args) {
+        try {
+            java.sql.Connection connection = koneksiLogin();
+            if (connection != null) {
+                System.out.println("Koneksi BerhasiL.");
+            } else {
+                System.out.println("Koneksi Gagal");
+            }
+        } catch (Exception e) {
+            System.out.println("Terjadi kesalahan" + e.getMessage());
+        }
+    }
+
+}
